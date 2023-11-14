c3e2fcc51166337fe2a3593138880be70d3bc639
smoochies <ogungbadeenitan@gmail.com>
2023-11-14 11:26:50 +0100

    lesson on functional componenet

---
+++ pages/about-us.js
@@ -0,0 +1,20 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+
+export default function Aboutus () {
+
+    const router = useRouter();
+
+    useEffect (() => {
+        router.push('contact-us')
+    },[])
+
+
+    return(
+        <>
+        
+        <h1>Your verification ID is {router.query.id} </h1>
+        </>
+    )
+}
